2352e6248829ea6c729aabd3a101899518042c36
russeltjahjadi <russel.tjahjadi@gmail.com>
2026-03-18 18:01:57 -0500

    Initialization for the project. Adding in git ignore as well

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {
+  res.send("Server is ready 123");
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on port http://localhost:3000");
+});
