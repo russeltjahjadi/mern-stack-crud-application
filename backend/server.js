6970c97968171b9745ac51828511607b45cf86b3
russeltjahjadi <russel.tjahjadi@gmail.com>
2026-04-06 21:42:30 -0500

    building for production

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
 app.use(express.json()); // middle layer - allows us to parse JSON data from the request body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*splat", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Connect to DB first, then start the server
 const startServer = async () => {
